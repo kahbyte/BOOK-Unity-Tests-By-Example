04d75253ada4802d895843f67db8ebaf022ec632
kaue.sousa <kaue.sousa@ab-inbev.com>
2023-06-27 22:59:33 -0300

    finished chapter 2

---
+++ 2. Manage Your Test Life Cycles/iOS Unity Tests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Unity Tests
+//
+//  Created by Kaue Sousa on 26/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
