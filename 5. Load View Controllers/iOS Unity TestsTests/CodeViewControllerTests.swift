949b5d5e12ed709ebc28e6f055c98711c07decb8
kaue.sousa <kaue.sousa@ab-inbev.com>
2023-06-30 22:30:17 -0300

    finished chapter 5
    Co-authored-by: NAME <beatrizsilv6@gmail.com>

---
+++ 5. Load View Controllers/iOS Unity TestsTests/CodeViewControllerTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+
+@testable import iOS_Unity_Tests
+
+final class CodeViewControllerTests: XCTestCase {
+    func test_loading() {
+        let sut = CodeViewController(data: "DUMMY")
+        
+    }
+}
